e17e938a3367e85435b5fc71de5557ae15c43015
Jose Diego Mendes da silva <122491768+Diegomendessilva@users.noreply.github.com>
2023-03-02 11:14:50 -0300

    Add files via upload
    testando

---
+++ Principal04.java
@@ -0,0 +1,23 @@
+
+public class Principal04 {
+
+	public static void main(String[] args) {
+		
+		byte x = 3;
+		short y = 4;
+		int z = 5;
+		long k = 6;
+		float l = 4;
+		double h = 5;
+		boolean t = true;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(h);
+		System.out.println(t);
+	}
+
+}
